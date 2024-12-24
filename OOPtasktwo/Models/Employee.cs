00a77f904ac66d6958a4ee2e0579987535305f38
zeeen <zeeen@DESKTOP-GP9JGLO>
2024-12-24 15:12:57 +0300

    Add project files.

---
+++ OOPtasktwo/Models/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtasktwo.Models
+{
+    public class Employee
+    {
+        public double Add( double a, double b)
+        {
+            return a + b;
+        }
+        public double Add( double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        public int Add(int a, int b, int c )
+        {
+            return a + b + c  ;
+        }
+        public double TotalSalary(double Salary,double workhour)
+        {
+            double hourRate = 10;
+            double calc =Salary+( workhour * hourRate);
+            return  calc;
+        }
+    }
+}
---
+++ OOPtasktwo/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Reflection;
+using System.Xml.Linq;
+using OOPtasktwo.Models;
+
+Employee emp = new Employee();
+
+//double a, b, c ,Salary, workhour;
+
+Console.WriteLine(" enter the first and second number");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(emp.Add(a, b));
+
+
+Console.WriteLine("enter the first , second and third numbers");
+ a = Convert.ToDouble(Console.ReadLine());
+ b = Convert.ToDouble(Console.ReadLine());
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(emp.Add(a, b, c));
+
+
+Console.WriteLine("enter the first , second and third numbers");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(emp.Add(x, y, z));
+
+
+Console.WriteLine("enter salary");
+double salary = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter workhour");
+double workhour= Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(emp.TotalSalary(salary,workhour));
